ce7af6557f04395c2edbaeb47a6ce9606e1162b8
Alexander Laskavtsev <las@agrain.ua>
2024-05-05 14:56:00 +0000

    update main

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "gke_cluster" {
-  source         = "github.com/vit-um/tf-google-gke-cluster"
+  source         = "github.com/laskavtsev-dev/terraform"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
   GKE_NUM_NODES  = 2
